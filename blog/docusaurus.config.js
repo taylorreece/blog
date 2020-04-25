93c44fa016056ff7a41c494cc36541ef54c9c64d
Taylor Reece <taylor.reece@gmail.com>
2020-04-25 12:45:13 -0500

    First commit for blog.

---
+++ blog/docusaurus.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  title: "Taylor's Blog",
+  tagline: "Taylor's Blog",
+  url: 'https://blog.reecemath.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Taylor's Blog",
+      logo: {
+        alt: 'My Site Logo',
+        src: 'https://avatars1.githubusercontent.com/u/3622586?s=400&u=92d81adf0d87e31ac26d0f335e154697c2e5d04a&v=4',
+      },
+      links: [
+        {
+          href: 'https://github.com/taylorreece',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/taylorreece/',
+            },
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/taylor.reece/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Taylor Reece`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/taylorreece/blog',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
