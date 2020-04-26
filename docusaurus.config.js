d70ff6d5129e6ac1b773997587db47102bce6534
Taylor Reece <taylor.reece@gmail.com>
2020-04-26 15:13:42 -0500

    Scones recipe.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ module.exports = {
           items: [
             {
               label: 'Blog',
-              to: 'blog',
+              to: '/',
             },
             {
               label: 'GitHub',
