43c4ccd9e9ee32594fd1e8fe522193cc9a0815a9
Taylor Reece <taylor.reece@prismatic.io>
2025-01-31 11:02:09 -0600

    Upgrade Docusaurus to 3.x

---
+++ docusaurus.config.ts
@@ -0,0 +1,92 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Taylor's Blog",
+  tagline: "Taylor's Blog",
+  url: "https://blog.reecemath.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+
+  organizationName: "taylorreece", // Usually your GitHub org/user name.
+  projectName: "blog", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: false,
+        blog: {
+          routeBasePath: "/",
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Taylor's Blog",
+      logo: {
+        alt: "My Site Logo",
+        src: "https://avatars1.githubusercontent.com/u/3622586?s=400&u=92d81adf0d87e31ac26d0f335e154697c2e5d04a&v=4",
+      },
+      items: [
+        {
+          href: "https://github.com/taylorreece/blog",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "/",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/taylorreece/",
+            },
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/taylor-reece/",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Taylor Reece`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
